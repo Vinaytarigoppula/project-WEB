d529f4652d97412a71845d833e1d2f9acec5459f
Vinaytarigoppula <vinaytarigoppula@gmail.com>
2024-11-02 12:36:01 +0530

    pusghing node modules and changes in backend

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+  //Step 1 - Make the get route work and render the index.ejs file.
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
