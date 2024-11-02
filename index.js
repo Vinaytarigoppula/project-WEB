9dd83aa8177457faebb6c5603a74c5b2f9ec9d7b
mukesh <mukeshpyla1312@gmail.com>
2024-11-02 14:09:11 +0530

    Updating after making changes to the interface

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("login.ejs");
+  res.render("Register.ejs");
   //Step 1 - Make the get route work and render the index.ejs file.
 });
 app.listen(port, () => {
